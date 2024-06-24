d7d76ea301942715c5f4e3662a1f498b7c9b8c89
Weixuan Ning <73421965+WeixuanPlant@users.noreply.github.com>
2024-06-24 16:10:56 -0500

    Update 1_ReadsDownload.sh

--- 00_Bioinformatic_GVCF_VCF/1_ReadsDownload.sh
+++ 00_Bioinformatic_GVCF_VCF/1_ReadsDownload.sh
@@ -1,15 +1,4 @@
-#!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=10
-#SBATCH --mem=100G
-#SBATCH --time=2-00:00:00
-#SBATCH --open-mode=append
-#SBATCH --mail-user=weixuan@iastate.edu
-#SBATCH --mail-type=ALL
-#SBATCH --output="job.vcf.%J.out"
-#SBATCH --job-name="Moving"
-#SBATCH --exclude=legion-[1-8]
-#SBATCH --array=1-3
+# setup directory and a list of SRA samples for downloading 
 
 dir=$(pwd)
 file=$dir/AD2list.txt
