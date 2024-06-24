c26356f749049e954660702c6254190edb08a5a3
Weixuan Ning <73421965+WeixuanPlant@users.noreply.github.com>
2024-06-24 16:11:37 -0500

    Update 1_ReadsDownload.sh

--- 00_Bioinformatic_GVCF_VCF/1_ReadsDownload.sh
+++ 00_Bioinformatic_GVCF_VCF/1_ReadsDownload.sh
@@ -1,7 +1,7 @@
 # setup directory and a list of SRA samples for downloading 
 
 dir=$(pwd)
-file=$dir/AD2list.txt
+file=$dir/ListofSample.txt
 
 sampledata=$(cat $file | sed -n ${SLURM_ARRAY_TASK_ID}p)
 echo "Samplebam = " $sampledata
@@ -10,6 +10,6 @@ module load  sra-tools
 
 #fasterq-dump SRR6311485
 
-#prefetch $sampledata --max-size 100G
+prefetch $sampledata --max-size 100G
 fasterq-dump --threads 8 --split-files $sampledata/$sampledata.sra
 gzip $sampledata*.fastq
